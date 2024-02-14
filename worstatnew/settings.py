5ea795108c6700bef77edbae0ad090f9cc687919
SolanoJason <pajason2000@outlook.com>
2024-02-14 12:50:24 -0500

    'python39'

--- worstatnew/settings.py
+++ worstatnew/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-    'rest_framework',
     'tailwind',
     'theme',
     'django_browser_reload',
