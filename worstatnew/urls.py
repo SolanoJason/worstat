000ac2c783e2bb2f150ba94563b10b0fbecd1406
SolanoJason <pajason2000@outlook.com>
2024-02-14 17:42:28 -0500

    'whitenoise'

--- worstatnew/urls.py
+++ worstatnew/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('', include('courses.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
